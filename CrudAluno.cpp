6475690b507b65a9cf38fe97cbca87fbb2b7c93f
MatheusFreire7 <cc21251@g.unicamp.br>
2022-12-03 13:48:32 -0300

    Crud arquivo concluido

--- CrudAluno.cpp
+++ CrudAluno.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <cstring>    // PARA USAR strcpy() e strcmp()
 #include <string.h>
+#include<stdio.h>
 using namespace std;
 #pragma warning(disable:4996) //Para desabilitar o erro 4996
 
@@ -28,7 +29,9 @@ void setQuantosAlunos(int quantidade)
 
 void quantidadeAlunosCadastrados()
 {
-    std::cout << getQuantosAlunos() + " \n";
+    cout << "Quantidade de Alunos: ";
+    cout << quantosAlunos;
+    cout << "\n\n";
 }
 
 void incluirAluno()
@@ -53,7 +56,8 @@ void incluirAluno()
             cout << "\n Digite a idade : ";
             cin >> aluno.idade;
             arq.write((char*)&aluno, sizeof(aluno));
-            cout << "Voce quer digitar mais registros aperte s para sim: ";
+            quantosAlunos++;
+            cout << "Voce quer digitar mais registros aperte "s" para sim: ";
             std::cin >> ch;  // LEITURA DA OPÇÃO DESEJADA
         }
         arq.close();
@@ -62,79 +66,109 @@ void incluirAluno()
 
 void excluirAluno()
 {
-    int ra;
-    char nome[31];
-    int idade;
-    std::cout << "Digite o Ra do aluno\n";
-    std::cin >> ra;
-    std::cin.ignore(80, '\n');// LIMPA BUFFER DO TECLADO
-    std::cout << "Digite o nome do aluno\n";
-    std::cin >> nome;
-    std::cin.ignore(80, '\n');// LIMPA BUFFER DO TECLADO
-    std::cout << "Digite a idade do aluno\n";
-    std::cin >> idade;
-    std::cin.ignore(80, '\n');// LIMPA BUFFER DO TECLADO
+    ifstream ifs("aluno.dat", ios::binary | ios::in);
+    ofstream ofs("temp.dat", ios::binary | ios::out);
     Aluno aluno;
-    aluno = { ra,"nome",idade };
-    int posicao = 0;
-    FILE* PtrAluno = fopen("Alunos.dat", "ab");
-    fflush(stdin);
-
-    //posicao = buscaBinariaAluno(aluno.ra);
-
-    if (posicao != 0) // se posição = 0 quer dizer que encontramos o registro de aluno
-    {
-        //std::cout << "Registro de aluno já registrado";
-        printf("Cadastre um aluno, para poder excluir");
-    }
-    else
-    {
-        printf("Aluno excluido com sucesso");
-    }
-    fclose(PtrAluno);
-}
-
-void alterarAluno()
-{
-    fstream iof("aluno.dat", ios::binary | ios::in | ios::out);
-    iof.seekg(0);
-    Aluno aluno;
-    bool achou = false;
-    if (!iof)
+    if (!ifs || !ofs)
     {
         cout << "Arquivo não foi possível ser aberto";
     }
     else
     {
         int ra;
-        int flag = 1;
-        cout << "Digite o Ra do Aluno para altera-lo:";
+ 
+        cout << "Digite o Ra para excluir o Aluno: ";
         cin >> ra;
-        cout << "Ra \tNome\tIdade\t\n";
-        while (iof.read((char*)&aluno, sizeof(aluno)) && achou == false)
+        while (ifs.read((char*)&aluno, sizeof(aluno)))
         {
-            if (aluno.ra == ra)
+            if (aluno.ra != ra)
             {
-                cout << "Registro encontrado: \n";
-                cout << aluno.ra << "\t";
-                cout << aluno.nome << "\t";
-                cout << aluno.idade << endl;
-                cout << "\n";
-                achou = true;
-                cout << "Digite o novo aluno para alterar: \n";
-                cout << "Ra: "; cin >> aluno.ra;
-                cout << "Nome: "; cin >> aluno.nome;
-                cout << "Idade: "; cin >> aluno.idade;
-                iof.seekp(sizeof(aluno),ios::cur);
-                //iof.seekg(sizeof(aluno), ios::cur);
-                iof.write((char*)&aluno, sizeof(aluno));
+                ofs.write((char*)&aluno, sizeof(aluno));
             }
         }
-        if (achou == false)
-            cout << "Registro não encontrado: \n";
-        iof.close();
+        quantosAlunos--;
+        ifs.close();
+        ofs.close();
+        remove("aluno.dat");
+        rename("temp.dat", "aluno.dat");
+    }
+
+}
+
+void alterarAluno()
+{
+    ifstream ifs("aluno.dat", ios::binary | ios::in);
+    ofstream ofs("temp.dat", ios::binary | ios::out);
+    Aluno aluno;
+    if (!ifs || !ofs)
+    {
+        cout << "Arquivo não foi possível ser aberto";
+    }
+    else
+    {
+        int ra;
+
+        cout << "Digite o Ra do Aluno que quer alterar: ";
+        cin >> ra;
+        while (ifs.read((char*)&aluno, sizeof(aluno)))
+        {
+            if (aluno.ra != ra)
+            {
+                ofs.write((char*)&aluno, sizeof(aluno));
+            }
+        }
+        ifs.close();
+        ofs.close();
+        remove("aluno.dat");
+        rename("temp.dat", "aluno.dat");
+    }
+
+    ofstream arq("aluno.dat", ios::binary | ios::app);
+    if (!arq)
+    {
+        std::cout << "Arquivo não pode ser aberto";
+    }
+    else
+    {
+        cout << "Digite o novo aluno para alterar: \n";
+        cout << "\n Digite o Ra : ";
+        cin >> aluno.ra;
+        cout << "\n Digite o Nome : ";
+        cin >> aluno.nome;
+        cout << "\n Digite a idade : ";
+        cin >> aluno.idade;
+        arq.write((char*)&aluno, sizeof(aluno));
+        quantosAlunos++;
+
+        arq.close();
     }
 }
+
+void exibirAlunos()
+{
+    Aluno aluno;
+
+    ifstream ifs("aluno.dat", ios::binary);
+    if (!ifs)
+    {
+        cout << "Arquivo não foi possível ser aberto";
+    }
+    else
+    {
+        cout << "Registros de alunos lidos: \n";
+        //cout << "Ra \tNome\tIdade\t\n";
+        while (ifs.read((char*)&aluno, sizeof(aluno)))
+        {
+            cout << aluno.ra << "\t";
+            cout << aluno.nome << "\t";
+            cout << aluno.idade << endl;
+            quantosAlunos++;
+        }
+        cout << "\n";
+        ifs.close();
+    }
+}
+
 
 void buscaAluno() //Busca o aluno pelo seu Ra, retornrá 0 se não encontrar o aluno
 {
@@ -148,8 +182,7 @@ void buscaAluno() //Busca o aluno pelo seu Ra, retornrá 0 se não encontrar o alu
     else
     {
         int ra;
-        int flag = 1;
-        cout << "Digite o Ra para bucar o Aluno: ";
+        cout << "Digite o Ra para buscar o Aluno: ";
         cin >> ra;
         while (ifs.read((char*)&aluno, sizeof(aluno)))
         {
@@ -163,8 +196,10 @@ void buscaAluno() //Busca o aluno pelo seu Ra, retornrá 0 se não encontrar o alu
                 achou = true;
             }
         }
-        if(achou == false)
-            cout << "Registro não encontrado: \n";
+        if (achou == false)
+        {
+            cout << "Registro não encontrado: \n\n";
+        }
         ifs.close();
     }
 
@@ -189,26 +224,22 @@ void lerAquivoAlunos()
     }
 }
 
-void gravarArquivoAlunos()
-{
-}
-
-
 short menu(void)  // MENU PRINCIPAL COM TODAS AS OPÇÕES
 {    
     short opcao;// ARMAZENA A OPCAO DO MENU
     do {
         std::cout << " Menu Crud\n\n\n";
-        std::cout << " [1] - Cadastrar registro.\n\n";
+        std::cout << " [1] - Cadastrar Aluno.\n\n";
         std::cout << " [2] - Excluir Aluno.\n\n";
-        std::cout << " [3] - Pesquisar aluno por Ra.\n\n";
-        std::cout << " [4] - Exibir quantidade de alunos Cadastrados.\n\n";
-        std::cout << " [5] - Alterar registro.\n\n";
+        std::cout << " [3] - Pesquisar Aluno por Ra.\n\n";
+        std::cout << " [4] - Exibir quantidade de Alunos Cadastrados.\n\n";
+        std::cout << " [5] - Alterar Aluno.\n\n";
+        std::cout << " [6] - Exibir Alunos.\n\n";
         std::cout << " [0] - Sair.\n\n";
         std::cout << " Entre a opcao desejada: ";
         std::cin >> opcao;  // LEITURA DA OPÇÃO DESEJADA
         std::cin.ignore(80, '\n'); // LIMPA BUFFER DO TECLADO
-    } while (opcao < 0 or 5 < opcao); // EVITA OPCÃO INEXISTENTE
+    } while (opcao < 0 or 6 < opcao); // EVITA OPCÃO INEXISTENTE
     return opcao; // RETORNA O NÚMERO DA OPÇÃO
 }
 
@@ -224,12 +255,13 @@ int main()
 	else
 	{
         cout << "Registros de alunos lidos: \n";
-		cout << "Ra \tNome\tIdade\t\n";
+		//cout << "Ra \tNome\tIdade\t\n";
 		while (ifs.read((char*)&aluno, sizeof(aluno)))
 		{
 			cout << aluno.ra << "\t";
 			cout << aluno.nome << "\t";
 			cout << aluno.idade << endl;
+            quantosAlunos++;
 		}
         cout << "\n";
 		ifs.close();
@@ -246,6 +278,7 @@ int main()
         case 3: buscaAluno(); break;
         case 4: quantidadeAlunosCadastrados();break;
         case 5: alterarAluno(); break;
+        case 6: exibirAlunos();break;
         }
     } while (opcao);
 
